89754a03e9d6503c617241b4b51357c3f40d2ee0
olmo <olmo1995@gmail.com>
2020-08-31 02:33:31 +0200

    fix:fixed script on navbar

--- js/app.js
+++ js/app.js
@@ -1,22 +1,19 @@
 var navbar = document.getElementById("header");
-var lastScroll = 0;
+var timer = null;
 
-window.addEventListener('DOMContentLoaded', _ => {
+document.addEventListener('DOMContentLoaded', _ => {
     // Event listener to add function to existing HTML DOM element
-    document.addEventListener('scroll', addBorder);
+    window.addEventListener('scroll', addBorder);
 });
 
-lastScroll = window.pageYOffset;
 function addBorder(e) {
     e.preventDefault();
 
-    console.log(lastScroll);
-
-    if(lastScroll != 0) { //if the page is scrolled
-        navbar.classList.add("border-bottom-added");
-    } 
-    else {
-        navbar.classList.remove("border-bottom-added");
+    if(timer !== null) {
+        clearTimeout(timer);      
+        navbar.classList.add("border-bottom-added");  
     }
-    
+    timer = setTimeout(function() {
+        navbar.classList.remove("border-bottom-added");
+    }, 250);
 }
