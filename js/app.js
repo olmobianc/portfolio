d3b4229bfc9e11b694a45a515ef55896ab25edd9
olmo <olmo1995@gmail.com>
2020-08-31 12:25:05 +0200

    feat:improved overall text

--- js/app.js
+++ js/app.js
@@ -1,19 +1,8 @@
 var navbar = document.getElementById("header");
-var timer = null;
 
-document.addEventListener('DOMContentLoaded', _ => {
-    // Event listener to add function to existing HTML DOM element
-    window.addEventListener('scroll', addBorder);
-});
-
+document.addEventListener('scroll', addBorder);
 function addBorder(e) {
-    e.preventDefault();
-
-    if(timer !== null) {
-        clearTimeout(timer);      
-        navbar.classList.add("border-bottom-added");  
-    }
-    timer = setTimeout(function() {
-        navbar.classList.remove("border-bottom-added");
-    }, 250);
+    e.preventDefault();   
+    
+    navbar.classList.add("border-bottom-added");  
 }
