ed6e1eeeb29c5fb2f923da5125ea4733dac0fcb7
olmo <olmo1995@gmail.com>
2020-09-01 17:52:30 +0200

    fix:fixed links on projects

--- js/app.js
+++ js/app.js
@@ -1,8 +1,11 @@
-var navbar = document.getElementById("header");
+//function that add border to navbar on scroll
+const navbar = document.getElementById("header");
 
 document.addEventListener('scroll', addBorder);
 function addBorder(e) {
     e.preventDefault();   
     
-    navbar.classList.add("border-bottom-added");  
+    navbar.classList.add("border-bottom-added");
 }
+
+
