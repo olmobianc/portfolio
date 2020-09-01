010bdfa6504316eadd8689407c29988f4d079d4b
olmo <olmo1995@gmail.com>
2020-09-01 18:49:19 +0200

    feat:first implementation of function make active class

--- js/app.js
+++ js/app.js
@@ -1,6 +1,11 @@
-//function that add border to navbar on scroll
+/**^ GLOBAL VARIABLES ***/
 const navbar = document.getElementById("header");
+const sections = document.getElementsByClassName("section-container");
+const navlinks = document.getElementsByClassName("nav-link");
 
+/*** FUNCTIONS DECLARATIONS ***/
+
+// Function that add border to navbar on scroll
 document.addEventListener('scroll', addBorder);
 function addBorder(e) {
     e.preventDefault();   
@@ -8,9 +13,8 @@ function addBorder(e) {
     navbar.classList.add("border-bottom-added");
 }
 
-//function that makes scrolling smooth
+// Function that makes scrolling smooth
 function makeSmoothScrolling() {
-    const navlinks = document.getElementsByClassName("nav-link");
     for(let i = 0; i < navlinks.length; i++) {
         navlinks[i].addEventListener('click', function(e) {
             e.preventDefault();
@@ -22,36 +26,34 @@ function makeSmoothScrolling() {
     }    
 }
 
-// Add class 'active' to section when near top of viewport
+// Function that add class 'active' to section when near top of viewport
 function makeActiveClasses() {
-    for(const section of sections) {
+    for (const section of sections) {
         const rect = section.getBoundingClientRect(); //get Rectangle container for each section
 
         //adding active class to the sections
-
         //mobile first
         let w = window.innerWidth;
         if(w < 600) {
-            if(rect.bottom <= window.innerHeight + 600) {
-                section.classList.add("your-active-class");
+            if (rect.bottom <= window.innerHeight + 600) {
+                section.classList.add("active-class");
                 var whichSection = section.getAttribute("id");
             }
             else {
-                section.classList.remove("your-active-class");
+                section.classList.remove("active-class");
             }
         //tablet and desktop    
         } else {
-            if(rect.bottom <= window.innerHeight) {
-                section.classList.add("your-active-class");
+            if (rect.bottom <= window.innerHeight) {
+                section.classList.add("active-class");
                 var whichSection = section.getAttribute("id");
             }
             else {
-                section.classList.remove("your-active-class");
+                section.classList.remove("active-class");
             }
         }
         
         //adding active class to the navbar Links
-        const navlinks = document.getElementsByClassName("navLinks");
         for(let i = 0; i < navlinks.length; i++) {
             if(whichSection == `section${i+1}`) {
                 navlinks[i].classList.add("active");
@@ -63,11 +65,15 @@ function makeActiveClasses() {
     }
 }
 
-/* FUNCTIONS CALLS */
+/*** FUNCTIONS CALLS ***/
 
 // Scroll to section on link click
 document.addEventListener('DOMContentLoaded', function(e) {
     makeSmoothScrolling();
 });
 
+// Set sections as active
+document.addEventListener('scroll', function(e) {
+    makeActiveClasses();
+});
 
