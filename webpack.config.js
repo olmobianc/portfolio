ebb826f8fce797ca64c95028b4a683cb9826ece0
olmo <olmo1995@gmail.com>
2020-09-03 13:34:16 +0200

    fix:fixed webpack config and added dev mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 const path = require("path");
 
 module.exports = {
-    mode = "devolpment",
-    entry = "./src/index.js",
-    output = {
+    mode: "development",
+    entry: "./src/index.js",
+    output: {
         filename: "main.js",
         path: path.resolve(__dirname, "dist")
     }
-}
+};
