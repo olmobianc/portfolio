fc4123de8f10905bc8d4ad46b80821b078bd91b2
olmo <olmo1995@gmail.com>
2020-09-09 20:07:20 +0200

    feat: added dev-server

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,37 +1,12 @@
-var HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require("path");
+const common = require("./webpack.common");
+const { merge } = require("webpack-merge");
 
-module.exports = {
+module.exports = merge(common, {
     mode: "production",
     entry: "./src/index.js",
     output: {
         filename: "main.[contentHash].js", //adding content Hash to prevent cache busting from browsers
         path: path.resolve(__dirname, "dist")
     },
-    module: {
-        rules: [
-            {
-                test: /\.scss$/i,
-                use: [
-                    "style-loader", //3. Inject styles into DOM
-                    "css-loader",   //2. Turns css into js
-                    "sass-loader"   //1. Turns sass into css
-                ], 
-            },
-            {
-                test: /\.(png|jpe?g|gif)$/i,
-                use: [
-                        {
-                            loader: 'file-loader',
-                            options: {
-                                outputPath: '/dist',  //needed to display the background image
-                            }    
-                        },
-                ],
-            },
-        ],
-    },
-    plugins: [new HtmlWebpackPlugin({
-        template: "./src/views/index.html"
-    })]
-};
+});
