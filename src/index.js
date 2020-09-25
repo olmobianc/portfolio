754eba7676376ab66d699f288dc82bfccd916ff1
olmo <olmo1995@gmail.com>
2020-09-25 17:40:20 +0200

    feat:finishing up tablet queries

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import { makeSmoothScrolling } from "../src/js/makeSmoothScrolling"
 import { addBorder } from "../src/js/addNavabarBorder"
-import { addResumeArrow } from "../src/js/resumeCanvas"
 import "./styles/main.scss"
 
 document.addEventListener('DOMContentLoaded', makeSmoothScrolling);
 document.addEventListener('scroll', addBorder);
-document.addEventListener('DOMContentLoaded', addResumeArrow);
 
 export { makeSmoothScrolling }
 export { addBorder }
-export { addResumeArrow }
 
