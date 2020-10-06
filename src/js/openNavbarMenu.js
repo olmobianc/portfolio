c82c1598b0f9c6cf1878a4c2e60be0533fd75786
olmo <olmo1995@gmail.com>
2020-10-06 12:43:38 +0200

    feat:first implementation of making the navbar collapse

--- src/js/openNavbarMenu.js
+++ src/js/openNavbarMenu.js
@@ -1,8 +1,10 @@
 const hamburger = document.getElementById("hamburger");
 const menu = document.getElementById("menu");
+const body = document.querySelector("body");
 
 hamburger.addEventListener("click", openMenu);
 
 export function openMenu() {
     menu.style.display = "block";
+    //body.style.overflow = "hidden";
 }
