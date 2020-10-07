0d89783da23a6d59d75150856dae76f4a8302d59
olmo <olmo1995@gmail.com>
2020-10-07 18:08:26 +0200

    feat:improving navbar scipts

--- src/js/openNavbarMenu.js
+++ src/js/openNavbarMenu.js
@@ -1,5 +1,6 @@
 const menuButton = document.getElementById("menuButton");
 const menu = document.getElementById("menu");
+const navLinks = document.getElementsByClassName("nav-link");
 const body = document.querySelector("body");
 let menuOpen = false;
 
@@ -7,14 +8,23 @@ menuButton.addEventListener("click", openMenu);
 
 export function openMenu() {
     if(menuOpen == false) {
-        menu.style.display = "block";
-        menuButton.classList.add('open');
-        
+        menu.classList.add("menu-active");
+        menuButton.classList.add("open");
         //body.style.overflow = "hidden";
         menuOpen = true;
+
+        for(let i = 0; i < navLinks.length; i++) {
+            navLinks[0].addEventListener("click", function() {
+                console.log(i)
+                menu.classList.remove("menu-active");
+                menuButton.classList.remove("open");
+            });
+        }
     }
     else {
+        menu.classList.remove("menu-active");
         menuButton.classList.remove("open");
+        //body.style.overflow = "visible";
         menuOpen = false;
     }
     
