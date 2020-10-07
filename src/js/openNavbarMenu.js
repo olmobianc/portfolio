33ea330a6400fa7ee5f2d7e93bbb199cdaebe310
olmo <olmo1995@gmail.com>
2020-10-07 19:05:35 +0200

    feat:improving navbar scripts

--- src/js/openNavbarMenu.js
+++ src/js/openNavbarMenu.js
@@ -2,30 +2,41 @@ const menuButton = document.getElementById("menuButton");
 const menu = document.getElementById("menu");
 const navLinks = document.getElementsByClassName("nav-link");
 const body = document.querySelector("body");
+const title = document.getElementsByClassName("header-logo");
 let menuOpen = false;
 
 menuButton.addEventListener("click", openMenu);
 
 export function openMenu() {
     if(menuOpen == false) {
+
+        //TABLET
         menu.classList.add("menu-active");
         menuButton.classList.add("open");
-        //body.style.overflow = "hidden";
+        body.classList.add("no-overflow");
         menuOpen = true;
 
+        //MOBILE
+        title[0].classList.add("hidden");
+
+        //LINKS
         for(let i = 0; i < navLinks.length; i++) {
             navLinks[0].addEventListener("click", function() {
-                console.log(i)
                 menu.classList.remove("menu-active");
                 menuButton.classList.remove("open");
+                body.classList.remove("no-overflow");
+                title[0].classList.remove("hidden");
             });
         }
     }
     else {
+        //TABLET
         menu.classList.remove("menu-active");
         menuButton.classList.remove("open");
-        //body.style.overflow = "visible";
+        body.classList.remove("no-overflow");
         menuOpen = false;
+
+        //MOBILE
+        title[0].classList.remove("hidden");
     }
-    
 }
