831db31e93c84f2678c07af5e30cdefc7a3ce218
olmo <olmo1995@gmail.com>
2020-10-17 15:42:27 +0200

    feat:added pdf to webpack.common.js

--- webpack.common.js
+++ webpack.common.js
@@ -10,7 +10,7 @@ module.exports = {
                 use: ["html-loader"],
             },
             {
-                test: /\.(pdf|png|jpe?g|gif)$/i,
+                test: /\.(png|jpe?g|gif|pdf)$/i,
                 use: 
                         {
                             loader: 'file-loader',
